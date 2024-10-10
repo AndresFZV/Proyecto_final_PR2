e7870eacb90fd236aafbd5672fb0428cd00eedc0
AndresFZV <141957774+AndresFZV@users.noreply.github.com>
2024-10-10 06:42:34 -0500

    Creación de los Enums

---
+++ co.edu.uniquindio.marketplace/src/main/java/co/edu/uniquindio/marketplace/marketplace/model/Enum/Categoria.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.marketplace.marketplace.model.Enum;
+
+public enum Categoria {
+    ROPA,
+    HOGAR,
+    ALIMENTOS,
+    LIBROS,
+    DEPORTES,
+    JUGUETES,
+    BELLEZA,
+    MASCOTAS,
+    TECNOLOGIA,
+    SALUD,
+    OTROS
+}
---
+++ co.edu.uniquindio.marketplace/src/main/java/co/edu/uniquindio/marketplace/marketplace/model/Enum/Estado.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.marketplace.marketplace.model.Enum;
+
+public enum Estado {
+    VENDIDO,
+    PUBLICADO,
+    CANCELADO
+}
