b8d39b1fe64e53944884c4f3d4a35ed7de5f933f
AndresFZV <141957774+AndresFZV@users.noreply.github.com>
2024-10-10 06:38:57 -0500

    Se eliminaron los comentarios

--- co.edu.uniquindio.marketplace/src/main/java/co/edu/uniquindio/marketplace/marketplace/model/Vendedor.java
+++ co.edu.uniquindio.marketplace/src/main/java/co/edu/uniquindio/marketplace/marketplace/model/Vendedor.java
@@ -4,16 +4,14 @@ import co.edu.uniquindio.marketplace.marketplace.model.builder.VendedorBuilder;
 
 public class Vendedor extends Persona {
 
-    // Constructor vacío
     public Vendedor() {
 
     }
-    // Constructor con todos sus atributos
     public Vendedor(String nombre, String apellido, String cedula,
                          String direccion, String telefono, String correo, Usuario usuario) {
         super(nombre, apellido, cedula, direccion, telefono, correo, usuario);
     }
-    // Builder de la clase Vendedor
+
     public static VendedorBuilder builder(){
         return new VendedorBuilder();
     }
